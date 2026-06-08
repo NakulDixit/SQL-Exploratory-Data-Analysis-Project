9ae7727decc9fe9aec26fe489bdfb840ee38ed72
Nakul Dixit <nakul.dixit24@gmail.com>
2026-06-08 12:28:15 +0530

    Add files via upload

---
+++ Scripts/Ranking.sql
@@ -0,0 +1,88 @@
+-- Which 5 products generate the highest revenue?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+
+-- Using Window Function
+'''
+SELECT *
+FROM (
+	SELECT
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue,
+	RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON f.product_key = p.product_key
+	GROUP BY p.product_name)t
+WHERE rank_products <= 5
+'''
+
+-- What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC
+
+
+-- Which 5 products subcategory generate the highest revenue?
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.subcategory
+ORDER BY total_revenue DESC
+
+-- What are the 5 worst-performing products subcategory in terms of sales?
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+GROUP BY p.subcategory
+ORDER BY total_revenue ASC
+
+
+-- Find the top 10 customers who have generated the highest revenue
+SELECT TOP 10
+c.customer_key,
+c.first_name,
+c.last_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY
+c.customer_key,
+c.first_name,
+c.last_name
+ORDER BY total_revenue DESC
+
+
+-- Find 3 customers with the fewest orders placed
+SELECT TOP 3
+c.customer_key,
+c.first_name,
+c.last_name,
+COUNT(order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY
+c.customer_key,
+c.first_name,
+c.last_name
+ORDER BY total_orders ASC
+
